0c229344739c974db9e06f81207d8f968a2b4b96
Success .N. Onyegbanokwu <75727917+Son-O-b-k@users.noreply.github.com>
2025-02-27 18:09:57 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
+  compiler: {
+    styledComponents: true,
+  },
   images: {
     remotePatterns: [
       {
